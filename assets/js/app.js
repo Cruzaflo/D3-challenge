d9fd327c58796f04dcf05b5eabceec14f798a508
Cruzaflo <64674430+Cruzaflo@users.noreply.github.com>
2020-09-08 19:53:40 -0700

    chart foundation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,33 @@
 // @TODO: YOUR CODE HERE!
+let width = parseInt(d3.select("#scatter").style("width"));
+
+let height = width - width / 4.0
+
+//margin spacing for graphg
+let margin = 20
+
+//space between words
+let labelArea = 110;
+
+//padding for the text at the bottom and left axes
+let textPaddingBottom = 40;
+let textPaddingLeft = 40;
+
+let svg = d3
+        .select("#scatter")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("class", "chart")
+
+let circleRadius;
+function circleGetRadius(){
+    if(width <= 530){
+        circleRadius = 5;
+    }
+    else{
+        circleRadius = 10;
+    }
+}
+
+circleGetRadius()
